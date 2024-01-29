22758e0682139214781e926da03a36e16f8c7aa0
Nicolay Byvshev <nic.law.ab@gmail.com>
2024-01-29 21:52:40 +0700

    rename the function and add the trim function. that trim zeros from beginnig

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const number = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
-const output = document.getElementById('output')
+const output = document.getElementById('output');
+const romeNum = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["c", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+];
 
 convertBtn.addEventListener('click', () => {
-    validationUserInput(number, output);
+    displayResult(number, output);
 });
 
-const validationUserInput = (input, elem) => {
+const displayResult = (input, elem) => {
     elem.style.fontSize = "2em";
     elem.style.color = "red";
     
@@ -15,6 +30,23 @@ const validationUserInput = (input, elem) => {
     } else if (+input.value < 1) {
         elem.textContent = "Please enter a number greater than or equal to 1";
     } else if (+input.value > 3999){
-        elem.textContent = "Please enter a number less than or equal to 3999"
+        elem.textContent = "Please enter a number less than or equal to 3999";
+    } else {
+        elem.style.fontSize = "3em";
+        elem.style.color = "gold";
+        elem.textContent = trimZerosFromBeginnig(input.value);
     }
 };
+
+const trimZerosFromBeginnig = (num) => {
+    let arrNum = num.split("");
+    let arabickNum = []; 
+    let tuggle = false;
+        
+    for(let i = 0; i < arrNum.length; i++) {      
+        if (+arrNum[i] !== 0) tuggle = true;
+        if (tuggle === true) arabickNum.push(+arrNum[i]);
+    }
+
+    return arabickNum;
+};  
